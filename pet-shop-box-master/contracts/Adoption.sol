359b611763dc31fd84f84d4f29ea99149384fc1b
wuuu <wyj1254157112@gmail.com>
2022-12-17 23:27:17 -0500

    revert adoption

--- pet-shop-box-master/contracts/Adoption.sol
+++ pet-shop-box-master/contracts/Adoption.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 contract Adoption {
 address[16] public adopters;
+address public nullAddr = 0x0000000000000000000000000000000000000000;
 // Adopting a pet
 function adopt(uint petId) public returns (uint) {
   require(petId >= 0 && petId <= 15);
@@ -10,6 +11,18 @@ function adopt(uint petId) public returns (uint) {
 
   return petId;
 }
+
+// Relisting a pet
+function relist(uint petId) public returns (uint) {
+  require(petId >= 0 && petId <= 15);
+
+  if (adopters[petId] == msg.sender){
+    adopters[petId] = nullAddr;
+
+    return 255;
+  }
+}
+
 // Retrieving the adopters
 function getAdopters() public view returns (address[16] memory) {
   return adopters;
